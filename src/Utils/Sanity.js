befd0a332a82fe8302b9667f8b428ffeab6895a7
kareem-n <kareemnasser1369@gmail.com>
2024-09-15 17:23:44 +0200

    add token auth

--- src/Utils/Sanity.js
+++ src/Utils/Sanity.js
@@ -2,13 +2,13 @@ import SanityClient from "@sanity/client";
 import ImageUrlBuilder from "@sanity/image-url";
 
 const client = new SanityClient({
-  projectId: 'nau6h6k0',
+  projectId: "nau6h6k0",
   dataset: "production",
   useCdn: true,
+  token:
+    "skYlX4rlokGaLdYNkuqA8i8eSXE0Nr9isFQaJnkEGbm5ooPQEzfc5fZox5wWMkroC1tm19ydLig4SKG62TqGXLs4JPh6JjZtVBVXQUnVXj21f8IQzc3ONCHLan6FfC9AEDiaIqh1FHiyDnUz01uY4Lt2Jk8aglhXL1r4tATlpmw70pgYpOex",
   apiVersion: "2024-09-14",
 });
-
-
 
 const builder = ImageUrlBuilder(client);
 
