8ca20770b81a84099f8f20543a42b1a2a358fcaf
mohamedFathey1907 <mf9021577@gmail.com>
2024-09-28 01:53:52 +0300

    FAQContant

---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PageBanner from '../../Components/pageBanner/PageBanner'
+
+const Contact = () => {
+  return (
+    <div>
+        <PageBanner/>
+        <div className='my-20 px-48'>
+            <div className='p-4 bg-gray-300'>
+                <h1 className='text-center font-extrabold text-2xl'>كسم الشناوي</h1>
+            </div>
+        </div>
+
+    </div>
+    
+    
+  )
+}
+
+export default Contact
