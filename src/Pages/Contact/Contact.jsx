a6bbaa3c10b8d7e7241aeb8a2a663802b08427d5
mohamedFathey1907 <mf9021577@gmail.com>
2024-09-29 02:02:17 +0300

    Contact Page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -104,6 +104,11 @@ const Contact = () => {
               </Reveal>
             </div>
         </div>
+        {/* MAP */}
+        <div className="map  px-32 pb-24 ">
+               <iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Mansoura+(Zahy)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps vehicle tracker</a></iframe>
+              </div>
+
     </div>
     
     
